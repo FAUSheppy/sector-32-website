83dbe0b741ef74bb994224f61b6ad31830eadbb4
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-06-22 17:53:54 +0200

    [git fast commit] 22. Jun 2021 - 17:53:54

--- config.py
+++ config.py
@@ -13,14 +13,16 @@ BG_SECTION_2 = "https://m.athq.de/ts-upload/Test/crystal-sector.png"
 TRAILER_URL="https://m.athq.de/ts-upload/yir_on_tilt.webm"
 VIDEO_ENCODING = "video/webm"
 
-SHORT_DESCRIPTION = "Decent into Sector 32 is a mystery game set in space, jump into your ship and only lifeline in increasingly dark and hostile enviroment of Sector 32 and either make your escape, uncover it's secrets or die trying."
+SHORT_DESCRIPTION = "Decent into Sector 32 is a desicion-based mystery game set in the far future. Jump into your ship and decent into  the increasingly dark and hostile enviroment of Sector 32 and either make your escape, uncover it's secrets or die trying."
 
 LONG_DESCRIPTION_RIGHT_COL = '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam laoreet enim vitae ante semper, et tristique quam maximus. Vestibulum suscipit dolor velit, eu pretium diam venenatis in. Donec ultricies dui eget massa viverra, nec tempor velit ultricies. Morbi convallis volutpat ipsum quis hendrerit. Suspendisse non commodo felis. Curabitur sit amet pulvinar eros. Nunc vitae ante vitae mi ornare consequat. Cras semper eros dolor, vitae vulputate tortor luctus non. In quis volutpat justo. Nunc dignissim urna vitae lectus vulputate sagittis. Nullam vestibulum, risus nec vestibulum
+Your carrier has been dispatch to investigate a large ship, that's been detected in the outer rings of the giant nebula in the nearby sector 32. An area notorious for the amount of ships going missing there.
 '''
 
 LONG_DESCRIPTION_LEFT_COL = '''
- Vivamus aliquam a mi in tincidunt. Donec convallis condimentum hendrerit. Sed id euismod elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vulputate sit amet risus eu eleifend. Praesent luctus interdum erat, dapibus rhoncus velit porttitor vitae. Sed congue odio vitae turpis varius faucibus. Suspendisse libero ex, convallis sodales turpis non, ullamcorper dignissim justo. Vivamus suscipit dignissim faucibus. Integer vitae finibus dui, quis aliquet neque. Quisque vel velit id nunc ultricies pellentesque a nec tortor. Etiam pulvinar, orci in dapibus ultricies, nibh turpis bibendum nunc, a malesuada tellus libero id lacus. Ut placerat mi eu purus sodales pharetra. Curabitur vel diam ac mi ornare pharetra vel id augue. Donec non mauris scelerisque, rutrum dui non, iaculis ipsum.
+You are an aspiring pilot in local security forces - one of the only ways to escape the poor and dangerous outer edges of the galaxy, by getting recruited int the regular navy of the Grand Federation.
+You copilots? Two veterans from the great war, demoted that is, parked in the middle of nowhere to be forgotten. <br>
+Out here, ships disappear every day. Most of them are never looked for. Out here, if you get lost, you <i> are </i> lost. But somebody seems to be in luck.
 '''
 
 PICTURE_1 = "https://m.athq.de/pictures/timeline/may-20-tunnel.jpg"
