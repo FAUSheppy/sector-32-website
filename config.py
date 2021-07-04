4e93f1083984875f1353f5df58bcee338241b6de
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-07-05 00:27:51 +0200

    [git fast commit] 05. Jul 2021 - 00:27:51

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# paralax layers front to back as urls #
+# parallax layers front to back as URLs #
 PARALAX_LAYERS = [
                 "https://images.potaris.de/media/Layer_0_edited.png?",
                 "https://images.potaris.de/media/Layer_1_editor.png?",
@@ -12,16 +12,14 @@ BG_SECTION_2 = "https://images.potaris.de/media/crystal-sector.png"
 TRAILER_URL="https://m.athq.de/ts-upload/yir_on_tilt.webm"
 VIDEO_ENCODING = "video/webm"
 
-SHORT_DESCRIPTION = "Decent into Sector 32 is a desicion-based mystery game set in the far future. Jump into your ship and decent into  the increasingly dark and hostile enviroment of Sector 32 and either make your escape, uncover it's secrets or die trying."
-
-LONG_DESCRIPTION_RIGHT_COL = '''
-Your carrier has been dispatch to investigate a large ship, that's been detected in the outer rings of the giant nebula in the nearby sector 32. An area notorious for the amount of ships going missing there.
-'''
+SHORT_DESCRIPTION = "Decent into Sector 32 is a decision-based mystery game, set in the far future. Jump into your ship and decent into  the increasingly dark and hostile environment of Sector 32."
 
 LONG_DESCRIPTION_LEFT_COL = '''
 You are an aspiring pilot in local security forces - one of the only ways to escape the poor and dangerous outer edges of the galaxy, by getting recruited int the regular navy of the Grand Federation.
-You copilots? Two veterans from the great war, demoted that is, parked in the middle of nowhere to be forgotten. <br>
-Out here, ships disappear every day. Most of them are never looked for. Out here, if you get lost, you <i> are </i> lost. But somebody seems to be in luck.
+You copilots? Two veterans from central, demoted that is, parked in the middle of nowhere to be forgotten.'''
+
+LONG_DESCRIPTION_RIGHT_COL = '''
+Out here, ships disappear every day. Most of them are never looked for - if you get lost, you <i> are </i> lost. But, today, somebody seems to be in luck, because your carrier has been dispatch to investigate an unusually large ship detected in the outer rings of the giant nebula at nearby sector 32. An area notorious for the amount of ships going missing there.
 '''
 
 PICTURE_1 = "https://images.potaris.de/media/may-20-tunnel.jpg?x=300&encoding=webp"
@@ -29,15 +27,15 @@ PICTURE_2 = "https://images.potaris.de/media/april-25-shield.jpg?x=300&encoding=
 PICTURE_3 = "https://images.potaris.de/media/ship-found.png?x=300&encoding=webp"
 PICTURE_4 = "https://images.potaris.de/media/bright-sector.png?x=300&encoding=webp"
 
-QUOTE = "Out of the places I've been, this, here, this is where you should truely abbandon all hope..."
+QUOTE = "Out of the places I've been, this, here, this is where you should truly abandon all hope..."
 
-CHOICES_SHORT_TEXT = "Choose your answers wisely but also pay attention to your surroudings. Following your veteran commarades may get you the end they want - but I may also leave those they don't care about out there to die."
+CHOICES_SHORT_TEXT = "Choose your answers wisely but also pay attention to your surroundings. Following your veteran comrades may get you the end they want - but I may also leave those they don't care about out there to die."
 CHOICES_PICTURE = "https://images.potaris.de/media/ui-example.png?x=300&encoding=webp"
 
 EXPLORE_THE_FIELD_TEXT = "Find countless secrets in the field, uncover the hidden messages from those who got lost before you - but be careful, you can't trust everything your see. "
 EXPLORE_PIC_1 = "https://images.potaris.de/media/ship-found.png?x=300&encoding=webp"
 EXPLORE_PIC_2 = "https://images.potaris.de/media/ship-found.png?x=300&encoding=webp"
 
-SURVIVAL_TEXT = "Take care of your ship and preserve your quipment. Damage to your ship or a wasted missle might come back to haunt you later."
+SURVIVAL_TEXT = "Take care of your ship and preserve your equipment. Damage to your ship or a wasted missle might come back to haunt you later."
 SURVIVAL_PIC_1 = ""
 SURVIVAL_PIC_2 = ""
