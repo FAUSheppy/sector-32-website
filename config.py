c67bf4fcffd694186f1475d1dfbd15466d03472c
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2021-07-24 23:09:19 +0200

    fix some typos

--- config.py
+++ config.py
@@ -19,11 +19,11 @@ VIDEO_ENCODING = "video/webm"
 SHORT_DESCRIPTION = "Decent into Sector 32 is a decision-based mystery game, set in the far future. Jump into your ship and decent into  the increasingly dark and hostile environment of Sector 32."
 
 LONG_DESCRIPTION_LEFT_COL = '''
-You are an aspiring pilot in local security forces - one of the only ways to escape the poor and dangerous outer edges of the galaxy, by getting recruited int the regular navy of the Grand Federation.
-You copilots? Two veterans from central, demoted that is, parked in the middle of nowhere to be forgotten.'''
+You are an aspiring pilot in local security forces - one of the only ways to escape the poor and dangerous outer edges of the galaxy, by getting recruited into the regular navy of the Grand Federation.
+Your copilots? Two veterans from central, demoted that is, parked in the middle of nowhere to be forgotten.'''
 
 LONG_DESCRIPTION_RIGHT_COL = '''
-Out here, ships disappear every day. Most of them are never looked for - if you get lost, you <i> are </i> lost. But, today, somebody seems to be in luck, because your carrier has been dispatch to investigate an unusually large ship detected in the outer rings of the giant nebula at nearby sector 32. An area notorious for the amount of ships going missing there.
+Out here, ships disappear every day. Most of them are never looked for - if you get lost, you <i> are </i> lost. But, today, somebody seems to be in luck, because your carrier has been dispatched to investigate an unusually large ship, detected in the outer rings of the giant nebula at nearby sector 32. An area notorious for the amount of ships going missing there.
 '''
 
 PICTURE_1 = "https://images.potaris.de/media/scan-with-target.png?x=500&encoding=webp"
@@ -33,16 +33,16 @@ PICTURE_4 = "https://images.potaris.de/media/bright-sector.png?x=500&encoding=we
 
 QUOTE = "Out of the places I've been, this, here, this is where you should truly abandon all hope..."
 
-CHOICES_SHORT_TEXT = "Choose your answers wisely but also pay attention to your surroundings. Following your veteran comrades may get you the end they want - but I may also leave those they don't care about out there to die."
+CHOICES_SHORT_TEXT = "Choose your answers wisely, but also pay attention to your surroundings. Following your veteran comrades may get you the end they want - but it may also leave those they don't care about out there to die."
 CHOICES_PICTURE_1 = "https://images.potaris.de/media/ui-example.png?x=500&encoding=webp"
 CHOICES_PICTURE_2 = "https://images.potaris.de/media/ue-flak-1.png?x=500&encoding=webp"
 CHOICES_SECTION_BG = "https://images.potaris.de/media/ue-what-is-real-sector.png?encoding=webp"
 
-EXPLORE_THE_FIELD_TEXT = "Find countless secrets in the field, uncover the hidden messages from those who got lost before you - but be careful, you can't trust everything your see. "
+EXPLORE_THE_FIELD_TEXT = "Find countless secrets in the field, uncover the hidden messages from those who got lost before you - but be careful, you can't trust everything you see."
 EXPLORE_PIC_1 = "https://images.potaris.de/media/ue-flak-2.png?x=500&encoding=webp"
 EXPLORE_PIC_2 = "https://images.potaris.de/media/ue-blue-white-field.png?encoding=webp"
 
-SURVIVAL_TEXT = "Take care of your ship and preserve your equipment. Damage to your ship or a wasted missle might come back to haunt you later."
+SURVIVAL_TEXT = "Take care of your ship and preserve your equipment. Damage to your ship or a single wasted missile might come back to haunt you later."
 SURVIVAL_PIC_1 = ""
 SURVIVAL_PIC_2 = ""
 
